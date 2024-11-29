3a1ff8551329265bc7af19bbce42d36d2684d44e
PhamHieu93 <hieuphampro456@gmail.com>
2024-11-29 15:48:52 +0700

    update icon

--- FrontEnd/src/app/buyprints/page.jsx
+++ FrontEnd/src/app/buyprints/page.jsx
@@ -1,4 +1,12 @@
 import styles from './page.module.css';
+import { IoMailOutline } from "react-icons/io5";
+import { LuPhone } from "react-icons/lu";
+import { FaRegBuilding } from "react-icons/fa";
+import { BiSolidArrowToLeft } from 'react-icons/bi';
+import visa from '@/assets/visa.png';
+import mastercard from '@/assets/mastercard.png';
+import momo from '@/assets/momo.png';
+import internetbanking from '@/assets/internetbanking.png'; 
 
 // function handleUploadClick() {
 //   document.getElementById('fileInput').click();
@@ -12,56 +20,46 @@ import styles from './page.module.css';
 // }
 const LoginSD = ()=> {
 return (
-    <div style={styles.container}>
-        <div style={styles.rectangle}>
-        <div style={styles.leftHalf}>
-            <div style={{ marginLeft: '30px', display: 'inline-block', fontSize: '16px', color: '#1488DB', marginTop: '5px' }}> &lt; Quay lại </div>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ marginLeft: '30px', display: 'inline-block', fontSize: '24px', color: 'black', marginTop: '50px' }}>Chọn khổ giấy: </div>
-            <button style={{ ...styles.button, padding: '5px', width: '60px', marginTop: '50px', borderRadius: '6px', marginLeft: '50px',backgroundColor:'white',border: '2px solid #1488DB', color:'#1488DB' }}>A3</button>
-            <button style={{ ...styles.button, padding: '5px', width: '60px', marginTop: '50px', borderRadius: '6px', marginLeft: '50px',backgroundColor:'white',border: '2px solid #1488DB', color:'#1488DB' }}>A4</button>
-            <button style={{ ...styles.button, padding: '5px', width: '60px', marginTop: '50px', borderRadius: '6px', marginLeft: '50px',backgroundColor:'white',border: '2px solid #1488DB', color:'#1488DB' }}>A5</button>
+    <div className={styles.container}>
+        <div className={styles.rectangle}>
+        <div className={styles.leftHalf}>
+        <div className={styles.item}>
+                    <BiSolidArrowToLeft size={20} className={styles.icon} />
+                    Quay lại
+                </div>
+            <div className={styles.titleContainer}>
+            <div className={styles.titleh1}>Chọn khổ giấy: </div>
+            <button className={`${styles.button} ${styles.titleh2}`}>A3</button>
+            <button className={`${styles.button} ${styles.titleh2}`}>A4</button>
+            <button className={`${styles.button} ${styles.titleh2}`}>A5</button>
             </div>
 
 
-            <div style={{ marginLeft: '30px', display: 'inline-block', fontSize: '32px', color: 'black', marginTop: '50px',fontWeight:'bold' }}>Đơn hàng của bạn</div>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ marginLeft: '250px', display: 'inline-block', fontSize: '14px', color: 'gray', marginTop: '15px' }}>Số lượng: </div>
-            <div style={{ marginLeft: '50px', display: 'inline-block', fontSize: '14px', color: 'gray', marginTop: '15px' }}>Gía tiền: </div>
+            <div className={styles.orderTitle}>Đơn hàng của bạn</div>
+            <div className={styles.orderContainer}>
+            <div className={styles.orderQuantity}>Số lượng </div>
+            <div className={styles.orderPrice}>Giá tiền </div>
             </div>
 
 
-            <div style={{ 
-                marginTop: '230px',
-                marginLeft: '15px',
-                width: '90%', 
-                border: '0.5px solid rgba(0, 0, 0, 0.7)', 
-                position: 'relative'
-            }}> </div>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ marginLeft: '50px', display: 'inline-block', fontSize: '20px', color: 'black', marginTop: '15px' }}>Tổng cộng: </div>
+            <div className={styles.totalContainer}>
+            <div className={styles.totalText}>Tổng cộng: </div>
             </div>
 
             </div>
-            <div style={styles.rightHalf}>
-            <div style={{ 
-                marginTop: '30px',
-                width: '100%', 
-                height: '100%',
-                backgroundColor: 'white',
-                position: 'relative',
-                boxShadow: '2px 2px 6px gray',
-                textAlign: 'left',
-            }}>
-              <div style={{ marginLeft: '40px', fontSize: '24px', color: '#032B91', marginTop: '30px',fontWeight:'bold'}}>Phương thức thanh toán</div>
-              <div style={{ marginLeft: '40px', fontSize: '16px', color: 'black',marginTop:'20px',fontWeight:'bold' }}>Chọn phương thức thanh toán</div>
-              <button style={styles.smallshowpttt}>Visa</button>
-              <button style={styles.smallshowpttt}>MasterCard</button>
-              <button style={styles.smallshowpttt}>MoMo</button>
-              <button style={styles.smallshowpttt}>Internet Banking</button>
-              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '5px', marginTop: '35px' }}>
-              <button style={{ ...styles.button, padding: '7px', width: '320px', borderRadius: '6px' }}>Xác nhận</button>
-              <button style={{ ...styles.button, padding: '7px', width: '320px',marginTop:'10px', borderRadius: '6px', backgroundColor: 'white', border: '2px solid #1488DB', color: '#1488DB' }}>Quay lại trang chủ</button>
+            <div className={styles.rightHalf}>
+            <div className={styles.paymentContainer}>
+              <div className={styles.paymentTitle}>Phương thức thanh toán</div>
+              <div className={styles.paymentSubtitle}>Chọn phương thức thanh toán</div>
+              <button className={styles.smallshowpttt}>
+              <Image src={visa} alt="visalogo" objectFit="cover" className={styles.logo}/>
+              Visa</button>
+              <button className={styles.smallshowpttt}>MasterCard</button>
+              <button className={styles.smallshowpttt}>MoMo</button>
+              <button className={styles.smallshowpttt}>Internet Banking</button>
+              <div className={styles.confirmButtons}>
+              <button className={styles.confirmButton}>Xác nhận</button>
+              <button className={styles.backHomeButton}>Quay lại trang chủ</button>
 </div>
       </div>
 
