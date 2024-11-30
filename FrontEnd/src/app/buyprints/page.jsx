efbc22c3bf7bca6e83495c45d6815f1acd154619
PhamHieu93 <hieuphampro456@gmail.com>
2024-11-30 11:44:18 +0700

    update buy print

--- FrontEnd/src/app/buyprints/page.jsx
+++ FrontEnd/src/app/buyprints/page.jsx
@@ -1,5 +1,7 @@
 import styles from './page.module.css';
 import Image from 'next/image';
+import Link from 'next/link';
+import pickpagebuy from "@/components/pickpagebuy/pickpagebuy";
 import { IoMailOutline } from "react-icons/io5";
 import { LuPhone } from "react-icons/lu";
 import { FaRegBuilding } from "react-icons/fa";
@@ -9,25 +11,22 @@ import mastercard from '@/assets/mastercard.png';
 import momo from '@/assets/momo.png';
 import internetbanking from '@/assets/internetbanking.png'; 
 
-// function handleUploadClick() {
-//   document.getElementById('fileInput').click();
-// }
-
-// function handleFileChange(event) {
-//   const file = event.target.files[0];
-//   if (file) {
-//     console.log('File selected:', file.name);
-//   }
-// }
 const LoginSD = ()=> {
+    const [selectedPaper, setSelectedPaper] = useState(null);
+
+    const handlePaperSelection = (type) => {
+      setSelectedPaper(type);
+    };
 return (
     <div className={styles.container}>
         <div className={styles.rectangle}>
         <div className={styles.leftHalf}>
-            <div className={styles.item}>
-                    <BiSolidArrowToLeft size={20} className={styles.icon} />
-                    Quay lại
-            </div>
+        <div className={styles.item}>
+            <Link href="/" className={styles.link}>
+            <BiSolidArrowToLeft size={20} className={styles.icon} />
+            <h3>Quay lại</h3>
+            </Link>
+        </div>
             <div className={styles.titleContainer}>
                 <div className={styles.titleh1}>Chọn khổ giấy: </div>
                 <button className={`${styles.button} ${styles.titleh2}`}>A3</button>
@@ -51,16 +50,30 @@ return (
         <div className={styles.rightHalf}>
             <div className={styles.paymentTitle}>Phương thức thanh toán</div>
             <div className={styles.subpaymentTitle}>Chọn phương thức thanh toán</div>
+            <div className={styles.buttonContainer}>
             <button className={`${styles.button} ${styles.title}`}>
             <Image src={visa} alt="visalogo" objectFit="cover" className={styles.logo}/>
             Visa
             </button>
-            <button className={styles.smallshowpttt}>MasterCard</button>
-            <button className={styles.smallshowpttt}>MoMo</button>
-            <button className={styles.smallshowpttt}>Internet Banking</button>
+            <button className={`${styles.button} ${styles.title}`}>
+            <Image src={mastercard} alt="MClogo" objectFit="cover" className={styles.logo}/>
+            Master Card
+            </button>
+            <button className={`${styles.button} ${styles.title}`}>
+            <Image src={momo} alt="momologo" objectFit="cover" className={styles.logo}/>
+            MoMo
+            </button>
+            <button className={`${styles.button} ${styles.title}`}>
+            <Image src={internetbanking} alt="iblogo" objectFit="cover" className={styles.logo}/>
+            Internet Banking
+            </button>
+            </div>
             <div className={styles.confirmButtons}>
                 <button className={styles.confirmButton}>Xác nhận</button>
-                <button className={styles.backHomeButton}>Quay lại trang chủ</button>
+                <button className={styles.backHomeButton}>
+                    <Link href="/"> Quay lại trang chủ
+                    </Link>
+                </button>
             </div>
       </div>
 
---
+++ FrontEnd/src/components/pickpagebuy/pickpagebuy.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const PaperBox = ({ paperType, initialQuantity = 0, price }) => {
+  const [quantity, setQuantity] = useState(initialQuantity);
+
+  const handleIncrease = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleDecrease = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleDelete = () => {
+    setQuantity(0);
+  };
+
+  if (quantity === 0) return null;
+
+  return (
+    <div className="paper-box">
+      <button onClick={handleDelete} className="delete-btn">X</button>
+      <span>{paperType}</span>
+      <div className="quantity">
+        <button onClick={handleDecrease}>-</button>
+        <span>{quantity}</span>
+        <button onClick={handleIncrease}>+</button>
+      </div>
+      <span>{price} VND</span>
+    </div>
+  );
+};
+
+export default PaperBox;
