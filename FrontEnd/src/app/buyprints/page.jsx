884b6f4eeb98f8286f39758dfe7a896bb7946db0
PhamHieu93 <hieuphampro456@gmail.com>
2024-11-30 18:07:56 +0700

    buy-print-complete

--- FrontEnd/src/app/buyprints/page.jsx
+++ FrontEnd/src/app/buyprints/page.jsx
@@ -3,6 +3,8 @@ import styles from './page.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 import { BiSolidArrowToLeft } from 'react-icons/bi';
+import { CiCirclePlus } from 'react-icons/ci';
+import { CiCircleMinus } from "react-icons/ci";
 import { HiOutlineNewspaper } from 'react-icons/hi';
 import { AiFillDelete } from 'react-icons/ai';
 import visa from '@/assets/visa.jpg';
@@ -53,67 +55,61 @@ const LoginSD = ()=> {
         (total, order) => total + order.quantity * parseFloat(order.price.replace('.', '')),
         0
       );
-
 return (
     <div className={styles.container}>
         <div className={styles.rectangle}>
         <div className={styles.leftHalf}>
-        <div className={styles.item}>
-            <Link href="/" className={styles.link}>
-            <BiSolidArrowToLeft size={20} className={styles.icon} />
-            <h3>Quay lại</h3>
-            </Link>
-        </div>
+            <div className={styles.item}>
+                <Link href="/" className={styles.link}>
+                <BiSolidArrowToLeft size={20} className={styles.icon} />
+                <h3>Quay lại</h3>
+                </Link>
+            </div>
             <div className={styles.titleContainer}>
                 <div className={styles.titleh1}>Chọn khổ giấy: </div>
                 <button
                     className={`${styles.button} ${styles.titleh2}`}
-                    onClick={() => handlePaperSelection('A3')}
+                    onClick={() => handlePaperSelection('Khổ giấy A3', '10000')}
                 >A3
                 </button>
                 <button
                     className={`${styles.button} ${styles.titleh2}`}
-                    onClick={() => handlePaperSelection('A4')}
+                    onClick={() => handlePaperSelection('Khổ giấy A4', '8000')}
                 >A4
                 </button>
                 <button
                     className={`${styles.button} ${styles.titleh2}`}
-                    onClick={() => handlePaperSelection('A5')}
+                    onClick={() => handlePaperSelection('Khổ giấy A5', '5000')}
                 >A5
                 </button>
             </div>
 
-
-                <div className={styles.orderTitle}>Đơn hàng của bạn</div>
-          <div className={styles.orderContainer}>
-          <div className={styles.orderQuantity}>Số lượng </div>
-          <div className={styles.orderPrice}>Giá tiền </div>
+            <div className={styles.orderTitle}> Đơn hàng của bạn</div>
+            <div className={styles.orderContainer}>
+            <div className={styles.orderQuantity}>Số lượng </div>
+            <div className={styles.orderPrice}>Giá tiền </div>
             </div>
 
+            {orders.map((order) => (
             <div className={styles.orderItemContainer}>
-            <AiFillDelete size={20} className={styles.titledelete} onClick={() => handleDeleteOrder('A3')} />
-            <HiOutlineNewspaper size={20} className={styles.title} />
-            <div className={styles.title}>Khổ giấy A3</div>
-
-          {orders.map((order) => (
-            <div key={order.paperType} className={styles.orderItem}>
-              <div>{order.paperType}</div>
+            <div key={order.paperType} className={styles.title}>
+                <h3 onClick={() => handleDeleteOrder(order.paperType)}><AiFillDelete size={20} className={styles.icon1}/></h3>
+                <div className={styles.title}><HiOutlineNewspaper size={20} className={styles.icon1} />{order.paperType}</div>
               <div>
-                <button onClick={() => handleQuantityChange(order.paperType, -1)}>-</button>
-                {order.quantity}
-                <button onClick={() => handleQuantityChange(order.paperType, 1)}>+</button>
+                <div className={styles.merge}>
+                    <h3 onClick={() => handleQuantityChange(order.paperType, -1)}><CiCircleMinus size={20} className={styles.icon2_1}/></h3>
+                    <div className={styles.quantity}>{order.quantity}</div>
+                    <h3 onClick={() => handleQuantityChange(order.paperType, 1)}><CiCirclePlus size={20} className={styles.icon2}/></h3>
+                </div>
               </div>
-              <div>{order.price}</div>
-              <button onClick={() => handleDeleteOrder(order.paperType)}>Xóa</button>
+              <div className={styles.titprice}>{order.quantity*order.price}</div>
+              </div>
             </div>
+
           ))}
-      </div>
-      <div className={styles.totalContainer}>
-        <div className={styles.totalText}>Tổng cộng: {totalPrice}</div>
-      </div>
+      <div className={styles.totalContainer}>Tổng cộng: {totalPrice}</div>
 
         </div>
-
         <div className={styles.rightHalf}>
             <div className={styles.paymentTitle}>Phương thức thanh toán</div>
             <div className={styles.subpaymentTitle}>Chọn phương thức thanh toán</div>
