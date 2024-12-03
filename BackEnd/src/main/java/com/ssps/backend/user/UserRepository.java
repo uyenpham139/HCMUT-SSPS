c148bc999857fa929e56bc81be8f6fc0b4087a91
DankChoir <timzek7@gmail.com>
2024-12-03 18:26:22 +0700

    [BACKEND, login]: basic login Controller

---
+++ BackEnd/src/main/java/com/ssps/backend/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ssps.backend.user;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
